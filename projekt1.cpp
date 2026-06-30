3d8f3a816cec70eed0fc8442507f3edf6ef1b43f
emzo <emzog@proton.me>
2026-06-30 20:35:06 +0200

    dodano sprawdzanie czy liczba jest pierwsza

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+bool czyPierwsza(int n) {
+    if (n < 2)
+        return false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
 
 long long silnia(int n) {
     long long wynik = 1;
@@ -23,9 +34,17 @@ int main() {
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+	cout << "1. Sprawdz silnie pierwszej liczby " << endl;        
+	cout << "2. Sprawdz czy liczba jest pierwsza" << endl;        
         cin >> wyjscie;
 	if (wyjscie == 1) {
 	    cout << "\nWynik: " << silnia(a) << endl;
+	}
+	if (wyjscie == 2) {
+    	    if (czyPierwsza(a))
+        	cout << "Liczba jest pierwsza." << endl;
+    	    else
+        	cout << "Liczba nie jest pierwsza." << endl;
 	}
     } while(wyjscie != 0);
     return 0;
